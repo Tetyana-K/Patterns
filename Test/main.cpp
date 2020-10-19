0a65432df12cb9d2dc1c12fc65180853c862c504
unknown <tanya.kyryk@gmail.com>
2020-10-20 00:31:25 +0300

    change main

--- Test/main.cpp
+++ Test/main.cpp
@@ -3,8 +3,9 @@ using namespace std;
 #include "Number.h"
 int main()
 {
-	Number n(5);
+	Number n(5), a(10);
 	cout << n.getNumber() << endl; //  
+	cout << a.getNumber()
 
 	Number m(7);
 	Number res = m.plus(n);
