12267b438dec7e0ae6fdb97409bfa37a5c8289bb
unknown <tanya.kyryk@gmail.com>
2020-10-20 00:24:51 +0300

    add explicit

--- Test/Number.h
+++ Test/Number.h
@@ -2,13 +2,13 @@
 class Number
 {
 public :
-	/*explicit*/ Number(int number = 0);
+	explicit Number(int number = 0);
 	int getNumber() const;
 	Number  plus(const Number & other); // this    +  other ---> result (Number)
 
 private:
 	int number;
-};
+
 
 inline int Number::getNumber() const
 {
